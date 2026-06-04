751d18564c9aedbc883efef61aa34b1076b17ed6
Nabila Hassya <nabilahassya99@gmail.com>
2026-06-04 15:32:19 +0700

    date-counter final baru

--- src/index.js
+++ src/index.js
@@ -18,17 +18,28 @@ function Counter() {
 
   const [step, setStep] = useState(1);
 
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
+
   return (
     <div>
       <div>
-        <button onClick={() => setCount((c) => c - step)}>-</button>
-        <span>Count: {count}</span>
-        <button onClick={() => setCount((c) => c + step)}>+</button>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+
+        <span>Step: {step}</span>
       </div>
       <div>
-        <button onClick={() => setStep((s) => s - 1)}>-</button>
-        <span>Step: {step}</span>
-        <button onClick={() => setStep((s) => s + 1)}>+</button>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <input type="text" value={count} onChange={(e) => setCount(Number(e.target.value))}/>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
       </div>
       <p>
         <span>
@@ -42,6 +53,11 @@ function Counter() {
           <span>{date.toDateString()}</span>
         </strong>
       </p>
+      { (count !== 0 || step !== 1) ?
+        (<div>
+        <button onClick={handleReset}>Reset</button>
+      </div>) : (null)
+      }
     </div>
   );
 }
